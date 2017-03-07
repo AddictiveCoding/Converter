b347658bf3cc4e7a3dcc1284ea615f93fea03114
itzraghav <RAGHAVGUPTA170101@YAHOO.COM>
2017-03-07 07:35:21 +0000

    Add files via upload
    A converter which converts Kilometer to metre

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int num ; 
+	printf("Enter a number ");
+	scanf("%d", &num) ;
+	
+	
+	printf("Now i am letting you know a secret...\n");
+	printf("Yu have just entered the number %d\n" , num);
+	return 0;
+}
